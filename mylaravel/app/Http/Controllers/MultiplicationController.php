933787943bb7d2d8a0ad550e317789b9a59107d8
TeerapolGOD <teerapol194@gmail.com>
2025-01-14 22:13:15 +0700

    Update MultiplicationController.php

--- mylaravel/app/Http/Controllers/MultiplicationController.php
+++ mylaravel/app/Http/Controllers/MultiplicationController.php
@@ -8,10 +8,10 @@ class MultiplicationController extends Controller
 {
     public function index(Request $request)
     {
-        // รับค่าจากฟอร์ม (multiplier) หรือค่าเริ่มต้น
-        $multiplier = $request->input('multiplier', 1);  // Default เป็น 1 ถ้าไม่ได้รับค่าจากฟอร์ม
+  
+        $multiplier = $request->input('multiplier', 1);
         
-        // คำนวณตารางสูตรคูณ
+     
         $multiplication_table = [];
         for ($i = 1; $i <= 12; $i++) {
             $multiplication_table[] = [
@@ -21,7 +21,7 @@ class MultiplicationController extends Controller
             ];
         }
 
-        // ส่งข้อมูลไปยัง View
+       
         return view('multiplication', compact('multiplication_table', 'multiplier'));
     }
 }
