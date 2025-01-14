eb8e0c3bf4980b2b98894ced6dbe1cf49ac22155
TeerapolGOD <teerapol194@gmail.com>
2025-01-14 19:09:32 +0700

    19.09

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -2,18 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyController;
+use App\Http\Controllers\LoginController;
 
-Route::get('/', function () {
-    return view('layouts.default');
-});
+Route::get('/login',
+    [LoginController::class, 'index']);
+
+
 
 Route::get('/hello', function () {
     return "<h1>Hello World!<h1>";
 });
 
-Route::get("/mycontroller/{id?}", [MyController::class,'myfunction']);
+Route::get("/mycontroller/{id?}", 
+    [MyController::class,'myfunction']);
 
-Route::post("/mycontroller/{id?}", [MyController::class,'myfunction']);
+Route::post("/mycontroller/{id?}", 
+    [MyController::class,'myfunction']);
+
+Route::get('/', function () {
+    return view('layouts.default');
+});
 
 Route::get('/hello/(id?)',
     function ($val="") {
