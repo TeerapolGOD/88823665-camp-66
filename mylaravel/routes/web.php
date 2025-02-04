e9b7e054356b58775ed35310d27dc7efa2b46762
TeerapolGOD <teerapol194@gmail.com>
2025-02-04 18:29:37 +0700

    q

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -10,17 +10,9 @@ use App\Http\Controllers\UserController;
 
 Route::get('/home',
     [HomeController::class, 'index']);
+
 Route::get('/',
     [HomeController::class, 'index']);
-
-Route::get('/login',
-    [LoginController::class, 'index']);
-
-Route::get('/register',
-    [RegisterController::class, 'index']);
-
-Route::post('/register', 
-    [RegisterController::class, 'create']);
 
 Route::get('/hello', function () {
     return "<h1>Hello World!<h1>";
@@ -44,5 +36,10 @@ Route::get('/hello/(id?)',
         return "<h1>Hello World $val</h1>";
     });
 
-Route::get('/user',
-    [UserController::class, 'index']);
+Route ::get('/login',[LoginController::class,'index']);
+Route ::get('/register',[RegisterController::class,'index']);
+Route ::post('/register',[RegisterController::class,'create']);
+Route ::get('/user',[UserController::class,'index']); 
+Route ::get('/user/{id}',[UserController::class,'edit']);
+Route ::put ('/user',[UserController::class,'saveEdit']);
+Route ::delete ('/user',[UserController::class,'delete']);
